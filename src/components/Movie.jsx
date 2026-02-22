e3f9b270f26935cc93717fa4f213ffebe2ad7c8a
devruhul <ruhulamin262001@gmail.com>
2026-02-22 14:23:43 +0000

    if a poster doaesnot have the image it will show placeholder image

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "../App.css";
 
 export default function Movie({ movie }) {
+  const poster =
+    movie.Poster && movie.Poster !== "N/A"
+      ? movie.Poster
+      : "https://via.placeholder.com/300x450?text=No+Image";
   return (
     <>
     <div className='movie-card'>
         <h3>{movie.Title}</h3>
         <p>{movie.Year}</p> 
-        <img src={movie.Poster} alt={movie.Title} />
+        <img src={poster} alt={movie.Title} />
     </div>
     </>
     
