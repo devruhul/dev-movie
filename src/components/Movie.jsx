09ba5961979b7508aeb205b00132afb1a0fb7f69
devruhul <ruhulamin262001@gmail.com>
2026-02-22 14:33:41 +0000

    fixed image responsive

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,12 +5,13 @@ export default function Movie({ movie }) {
   const poster =
     movie.Poster && movie.Poster !== "N/A"
       ? movie.Poster
-      : "https://via.placeholder.com/300x450?text=No+Image";
+      : "https://placehold.co/300x400?text=No+Image+Available&font=roboto&bg=cccccc";
   return (
     <>
     <div className='movie-card'>
         <h3>{movie.Title}</h3>
         <p>{movie.Year}</p> 
+        <p>{movie.Type}</p>
         <img src={poster} alt={movie.Title} />
     </div>
     </>
